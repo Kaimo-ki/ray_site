d54e3425b9728a5332ed872a6007b14da251912c
Molka <nurlankuat38@gmail.com>
2026-06-23 02:03:05 +0600

    Use Railway API by default

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// When Ray API is deployed, put the backend URL here.
-// Example: window.RAY_API_URL = "https://api.example.com";
-window.RAY_API_URL = "";
+window.RAY_API_URL = "https://ray-api-production.up.railway.app";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ray-web-v4";
+const CACHE_NAME = "ray-web-v5";
 const ASSETS = [
   "/ray_site/",
   "/ray_site/index.html",
