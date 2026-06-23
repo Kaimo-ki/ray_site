defa98e86a902059d6c98072dabb40509984d2d7
Molka <nurlankuat38@gmail.com>
2026-06-24 02:43:42 +0600

    Configure Supabase auth

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.RAY_API_URL = "https://ray-api-production.up.railway.app";
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://dtrqvzlrpvcjbpawxqfx.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_ltIFHvBKr7qPtl4hooBsgQ_C-WCnebW";
